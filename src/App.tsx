44b54221976a25021a2b5337ed8d0683c51dd753
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-08-07 11:10:52 +0200

    navbar, aside, and main is done

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from "./components/NavBar" 
+
+function App() {
+  
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      >
+
+      <GridItem gridArea="nav" bg="tomato">
+      <NavBar/>
+      </GridItem>
+      <Show above="lg">
+      <GridItem gridArea="aside" bg="green">
+      aside
+      </GridItem>
+      </Show>
+      <GridItem gridArea="main" bg="orange">
+      main
+      </GridItem>
+    
+    </Grid>
+    
+  )
+}
+
+export default App
